4d7ede73caf5394ef4beb86dddff62129bb182b1
Harriet Ayugi <clerryha@gmail.com>
2020-09-03 17:59:05 +0300

    add stripe backend integration with nodejs/express

--- client/src/components/stripe-button/stripeButton.jsx
+++ client/src/components/stripe-button/stripeButton.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import axios from "axios";
 import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51HCWgoDXlzB9rNQrMUDKrrjkjiaYktY8vPugw1ZORD1qTStzy2VZUAM4ZSU2bNhIQcQdcQx6rSrzrUGJvq2yWvB000KF8ctgGC";
-  const onToken = (token) => {
-    alert("Payment Successful");
-  };
+   const onToken = (token) => {
+     axios({
+       url: "payment",
+       method: "post",
+       data: {
+         amount: priceForStripe,
+         token: token,
+       },
+     })
+       .then((response) => {
+         alert("succesful payment");
+       })
+       .catch((error) => {
+         console.log("Payment Error: ", error);
+         alert(
+           "There was an issue with your payment! Please make sure you use the provided credit card."
+         );
+       });
+   };
   return (
     <StripeCheckout
       label="PAY NOW"
