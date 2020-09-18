d7d7ce11c456cf28eebeb594d8cf2b82125ec252
Harriet Ayugi <clerryha@gmail.com>
2020-09-18 21:12:56 +0300

    change image size for PWA

--- client/src/redux/directory/directoryReducer.js
+++ client/src/redux/directory/directoryReducer.js
@@ -2,13 +2,13 @@ const INITIAL_STATE = {
   sections: [
     {
       title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
       id: 1,
       linkUrl: "shop/hats",
     },
     {
       title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      imageUrl: "https://i.ibb.co/zX34tqh/jacket7.jpg",
       id: 2,
       linkUrl: "shop/jackets",
     },
